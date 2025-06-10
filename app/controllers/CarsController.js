19413c8640ba7808d65c0e587beb6b62089e3fca
Jeremy Fowler <jembyfowler@gmail.com>
2025-06-10 10:49:28 -0600

    drawing cars

--- app/controllers/CarsController.js
+++ app/controllers/CarsController.js
@@ -1,12 +1,20 @@
+import { AppState } from "../AppState.js";
 import { carsService } from "../services/CarsService.js";
 import { Pop } from "../utils/Pop.js";
 
 export class CarsController {
   constructor() {
     console.log('CARS CONTROLLER IS READY 🏎️');
+    AppState.on('cars', this.drawCars)
     this.getCars()
   }
-
+  drawCars() {
+    const cars = AppState.cars
+    let carsContent = ''
+    cars.forEach(car => carsContent += car.listingHTMLTemplate)
+    const carListingsElem = document.getElementById('car-listings')
+    carListingsElem.innerHTML = carsContent
+  }
   async getCars() {
     try {
       await carsService.getCars()
@@ -14,4 +22,6 @@ export class CarsController {
       Pop.error(error, 'UH OH!', '<b>Could not get those cars!</b>')
     }
   }
+
+
 }
